e2a5a68013fba3322d8105470c102c09f521210c
Saint <16396306+SaintGeele@users.noreply.github.com>
2024-09-04 00:45:54 -0400

    added more lambda func

--- IaC/main.tf
+++ IaC/main.tf
@@ -27,8 +27,57 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+resource "aws_iam_policy" "iam_policy_for_cloud_resume_project" {
+  
+  name = "aws_iam_policy_for_terraform_resume_project_policy"
+  path = "/"
+  description = "AWS IAM Policy for managing the resume project role"
+    policy = jsonencode(
+    {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                  "Action": [
+                      "logs:CreateLogGroup",
+                      "logs:CreateLogStream",
+                      "logs:PutLogEvents"
+                    ],
+                    "Resource" : "arn:aws:logs:*:*:*",
+                    "Effect": "Allow"
+                  },
+                  {
+                    "Effect" : "Allow"
+                    "Action" : [
+                    "dynamodb:UpdateItem",
+                    "dynamodb:GetItem"
+                  ],
+                  "Resource" : "arn:aws:dynamodb:*:*:table/cloudresume"
+                },
+              ]
+        })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_role_to_iam_policy" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_cloud_resume_project.arn
+}
+
 data "archive_file" "zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda/"
   output_path = "${path.module}/packedlambda.zip"
+}
+
+resource "aws_lambda_function_url" "url1" {
+  function_name      = aws_lambda_function.myfunc.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
