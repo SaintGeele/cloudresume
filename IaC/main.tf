6b21eca4e72567100df4a1e46bd1299b095109cc
Saint <16396306+SaintGeele@users.noreply.github.com>
2024-11-25 03:59:58 -0500

    updates

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_function" "myfunc" {
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = data.archive_file.zip.output_base64sha256
+  filename         = data.archive_file.zip_the_python_code.output_path
+  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "func.handler"
   runtime          = "python3.8"
 }
-  
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -27,45 +27,46 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_iam_policy" "iam_policy_for_cloud_resume_project" {
-  
-  name = "aws_iam_policy_for_terraform_resume_project_policy"
-  path = "/"
+resource "aws_iam_policy" "iam_policy_for_resume_project" {
+
+  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+  path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
     policy = jsonencode(
     {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                  "Action": [
-                      "logs:CreateLogGroup",
-                      "logs:CreateLogStream",
-                      "logs:PutLogEvents"
-                    ],
-                    "Resource" : "arn:aws:logs:*:*:*",
-                    "Effect": "Allow"
-                  },
-                  {
-                    "Effect" : "Allow"
-                    "Action" : [
-                    "dynamodb:UpdateItem",
-                    "dynamodb:GetItem"
-                  ],
-                  "Resource" : "arn:aws:dynamodb:*:*:table/cloudresume"
-                },
-              ]
-        })
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+			      "dynamodb:GetItem"
+          ],
+          "Resource" : "arn:aws:dynamodb:*:*:table/resume-challenge"
+        },
+      ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_iam_role_to_iam_policy" {
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   role = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_policy_for_cloud_resume_project.arn
+  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
+  
 }
 
-data "archive_file" "zip" {
+data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda/"
-  output_path = "${path.module}/packedlambda.zip"
+  source_file = "${path.module}/lambda/func.py"
+  output_path = "${path.module}/lambda/func.zip"
 }
 
 resource "aws_lambda_function_url" "url1" {
@@ -79,13 +80,5 @@ resource "aws_lambda_function_url" "url1" {
     allow_headers     = ["date", "keep-alive"]
     expose_headers    = ["keep-alive", "date"]
     max_age           = 86400
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "geele-cloud-resume"         # Name of the S3 bucket
-    key            = "Users/saintclaire/Desktop/AWS Cloud Resume/aws-cloud-resume-challenge-main/cloudresume/IaC/terraform.tfstate" # Path to store the state file
-    region         = "us-east-1"                # AWS Region
   }
 }
